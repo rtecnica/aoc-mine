58bc642c2ee71a2a036632ebf464a9c9b9f2f608
imaldonado <imaldonado@wiseconn.com>
2023-11-25 18:32:17 -0300

    Update

--- scripts/addModule.sh
+++ scripts/addModule.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 WORKING_DIR=$(pwd)
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 mkdir $1
 cd $1
 $SCRIPT_DIR/addmodule.sh c
+mv $SCRIPT_DIR/.clang-format c/
 stack new h
 mkdir r
 cd r
