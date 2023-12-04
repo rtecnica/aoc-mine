db7cb63cb4f4bbdc050cf0cb92e7eec2905974ea
rtecnica <imaldonado@ing.uchile.cl>
2023-12-04 00:49:54 -0300

    Updat

--- scripts/addModule.sh
+++ scripts/addModule.sh
@@ -3,10 +3,12 @@
 WORKING_DIR=$(pwd)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+mkdir $1
+cd $1
 $SCRIPT_DIR/addmodule.sh c
 stack new h
 mkdir r
 cd r
 cargo init
 cd ..
-
+cd $WORKING_DIR
