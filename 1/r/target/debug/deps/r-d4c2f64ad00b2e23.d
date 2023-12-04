261310478ed3c8042d1e2c7f464568b80547f794
rtecnica <imaldonado@ing.uchile.cl>
2023-12-04 10:04:50 -0300

    Added 1

---
+++ 1/r/target/debug/deps/r-d4c2f64ad00b2e23.d
@@ -0,0 +1,5 @@
+/home/rtecnica/Projects/aoc-mine/1/r/target/debug/deps/r-d4c2f64ad00b2e23.rmeta: src/main.rs
+
+/home/rtecnica/Projects/aoc-mine/1/r/target/debug/deps/r-d4c2f64ad00b2e23.d: src/main.rs
+
+src/main.rs:
