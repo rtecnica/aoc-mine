1b706e1ab1cf3832f3212c91b6bf083a564af35f
imaldonado <imaldonado@wiseconn.com>
2023-12-08 22:02:11 -0300

    Haskell fixes

--- 2023/1.1/h/src/Lib.hs
+++ 2023/1.1/h/src/Lib.hs
@@ -3,46 +3,24 @@ module Lib
   )
 where
 
-import qualified Data.Char as C
 import qualified Data.Text as T
+import qualified Data.Text.ICU.Char as C
+import qualified Data.Tuple as P
 
-parseCalFw :: T.Text -> Maybe Int
-parseCalFw line =
-  case T.uncons line of
+parseCalLine :: (T.Text -> Maybe (Char, T.Text)) -> T.Text -> Maybe Int
+parseCalLine func line =
+  case func line of
     Nothing -> Nothing
-    Just (c, t) -> case parseAux c of
-      Nothing -> parseCalFw t
+    Just (c, t) -> case C.digitToInt c of
+      Nothing -> parseCalLine func t
       Just i -> Just i
-
-parseCalBw :: T.Text -> Maybe Int
-parseCalBw line =
-  case T.unsnoc line of
-    Nothing -> Nothing
-    Just (t, c) -> case parseAux c of
-      Nothing -> parseCalBw t
-      Just i -> Just i
-
-parseAux :: C.Char -> Maybe Int
-parseAux c =
-  if C.isDigit c
-    then Just (C.digitToInt c)
-    else Nothing
-
-maybeSum :: Maybe Int -> Maybe Int -> Maybe Int
-maybeSum Nothing _ = Nothing
-maybeSum _ Nothing = Nothing
-maybeSum (Just a) (Just b) =
-  Just (a + b)
-
-maybeMul :: Maybe Int -> Maybe Int -> Maybe Int
-maybeMul Nothing _ = Nothing
-maybeMul _ Nothing = Nothing
-maybeMul (Just a) (Just b) =
-  Just (a * b)
 
 parseCalVal :: T.Text -> Maybe Int
 parseCalVal line =
-  maybeSum (maybeMul firstVal (Just 10)) secondVal
+  (+) <$> interVal <*> secondVal
   where
     firstVal = parseCalFw line
     secondVal = parseCalBw line
+    interVal = (* 10) <$> firstVal
+    parseCalFw = parseCalLine T.uncons
+    parseCalBw = parseCalLine ((fmap . fmap) P.swap T.unsnoc)
