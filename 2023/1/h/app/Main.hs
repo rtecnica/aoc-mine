99236dbf4a3ebf31e2c78db345f726a1b502e885
rtecnica <imaldonado@ing.uchile.cl>
2023-12-09 18:14:15 -0300

    Completed 1:1 in Haskell

--- 2023/1/h/app/Main.hs
+++ 2023/1/h/app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
-import Lib
+import qualified Data.Maybe as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Lib (parseCalVal)
 
 main :: IO ()
-main = someFunc
+main = do
+  content <- TIO.readFile "../input.txt"
+  let lns = T.splitOn (T.pack "\n") content
+  let result = sum (map (M.fromMaybe 0 . parseCalVal) lns)
+  print result
--- 2023/1/h/src/Lib.hs
+++ 2023/1/h/src/Lib.hs
@@ -1,6 +1,48 @@
 module Lib
-    ( someFunc
-    ) where
+  ( parseCalVal,
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Char as C
+import qualified Data.Text as T
+
+parseCalFw :: T.Text -> Maybe Int
+parseCalFw line =
+  case T.uncons line of
+    Nothing -> Nothing
+    Just (c, t) -> case parseAux c of
+      Nothing -> parseCalFw t
+      Just i -> Just i
+
+parseCalBw :: T.Text -> Maybe Int
+parseCalBw line =
+  case T.unsnoc line of
+    Nothing -> Nothing
+    Just (t, c) -> case parseAux c of
+      Nothing -> parseCalBw t
+      Just i -> Just i
+
+parseAux :: C.Char -> Maybe Int
+parseAux c =
+  if C.isDigit c
+    then Just (C.digitToInt c)
+    else Nothing
+
+maybeSum :: Maybe Int -> Maybe Int -> Maybe Int
+maybeSum Nothing _ = Nothing
+maybeSum _ Nothing = Nothing
+maybeSum (Just a) (Just b) =
+  Just (a + b)
+
+maybeMul :: Maybe Int -> Maybe Int -> Maybe Int
+maybeMul Nothing _ = Nothing
+maybeMul _ Nothing = Nothing
+maybeMul (Just a) (Just b) =
+  Just (a * b)
+
+parseCalVal :: T.Text -> Maybe Int
+parseCalVal line =
+  maybeSum (maybeMul firstVal (Just 10)) secondVal
+  where
+    firstVal = parseCalFw line
+    secondVal = parseCalBw line
