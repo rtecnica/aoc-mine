eded01e924eb216a28da0352ba8edf698ef1a0bb
rtecnica <imaldonado@ing.uchile.cl>
2023-12-10 20:47:16 -0300

    Rust clean up

--- 2023/1/r/target/debug/deps/r-c4fb63bfb5361fc7.d
+++
@@ -1,5 +0,0 @@
-/home/rtecnica/Projects/aoc-mine/1/r/target/debug/deps/r-c4fb63bfb5361fc7.rmeta: src/main.rs
-
-/home/rtecnica/Projects/aoc-mine/1/r/target/debug/deps/r-c4fb63bfb5361fc7.d: src/main.rs
-
-src/main.rs:
