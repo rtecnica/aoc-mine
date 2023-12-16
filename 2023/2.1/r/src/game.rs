738a0f803776e71ef720a90bdc00a12e04033d74
imaldonado <imaldonado@wiseconn.com>
2023-12-16 03:57:40 -0300

    Completed 2.1 in Rust

--- 2023/2.1/r/src/game.rs
+++ 2023/2.1/r/src/game.rs
@@ -1,15 +1,64 @@
-struct Bag {
-    red: u8,
-    green: u8,
-    blue: u8,
+pub struct Bag {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
 }
 
-struct Game {
-    id: u8,
-    sets: Option<Vec<Bag>>,
+pub struct Game {
+    pub id: u8,
+    sets: Vec<Bag>,
 }
 
-fn from_string(line: &str) -> Game {
-    let new_game = Game { id: 1, sets: None };
+pub fn from_string(line: &str) -> Game {
+    let mut new_game = Game {
+        id: 1,
+        sets: vec![],
+    };
+
+    let mut line_chunks = line.split(':');
+    new_game.id = line_chunks
+        .next()
+        .unwrap()
+        .split(' ')
+        .next_back()
+        .unwrap()
+        .parse()
+        .unwrap_or(0);
+    new_game.sets = parse_sets(line_chunks.next().unwrap());
     return new_game;
+}
+
+fn parse_sets(substr: &str) -> Vec<Bag> {
+    let mut sets: Vec<Bag> = vec![];
+    for set_str in substr.trim().split(';') {
+        let mut bag = Bag {
+            red: 0,
+            blue: 0,
+            green: 0,
+        };
+        for bag_str in set_str.trim().split(',') {
+            let mut bag_str_items = bag_str.trim().split(' ');
+            let num = bag_str_items.next().unwrap();
+            match bag_str_items.next().unwrap() {
+                "red" => bag.red = num.parse().unwrap(),
+                "blue" => bag.blue = num.parse().unwrap(),
+                "green" => bag.green = num.parse().unwrap(),
+                _ => println!("Somthings gone wrong!"),
+            }
+        }
+        sets.push(bag);
+    }
+    sets
+}
+
+pub fn is_game_possible(bag: &Bag, game: &Game) -> bool {
+    let get_colors = |g: &Bag| vec![g.red, g.blue, g.green].into_iter();
+    *(&game.sets[..]
+        .into_iter()
+        .map(|x: &Bag| {
+            get_colors(&x)
+                .zip(get_colors(bag))
+                .fold(true, |acc, (a, b)| (a <= b) && acc)
+        })
+        .fold(true, |acc, i| i && acc))
 }
--- 2023/2.1/r/src/main.rs
+++ 2023/2.1/r/src/main.rs
@@ -1,8 +1,21 @@
 use std::fs::read_to_string;
 
 mod game;
+use game::*;
 
 fn main() {
-    let running_sum: u32 = read_to_string("../input.txt");
-    println!("Hello, world!");
+    let ref_bag = Bag {
+        red: 12,
+        blue: 14,
+        green: 13,
+    };
+
+    let mut running_sum: u32 = 0;
+    for line in read_to_string("../input.txt").unwrap().lines() {
+        let curr_game = from_string(line);
+        if is_game_possible(&ref_bag, &curr_game) {
+            running_sum += curr_game.id as u32;
+        }
+    }
+    println!("{}", running_sum);
 }
