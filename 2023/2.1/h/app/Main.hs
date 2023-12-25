2f3cfa491e09d3d1568d8aa09775011f853ec997
rtecnica <imaldonado@ing.uchile.cl>
2023-12-25 03:13:45 -0300

    Completed 2.1 in Haskell

--- 2023/2.1/h/app/Main.hs
+++ 2023/2.1/h/app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import Lib
 
+refBag :: Bag
+refBag = Bag 12 13 14
+
 main :: IO ()
-main = someFunc
+main = do
+  content <- TIO.readFile "../input.txt"
+  let lns = T.lines content
+  let gms = filter (isGamePossible refBag) (map parseGame lns)
+  let sum_index = sum (map idNum gms)
+  print sum_index
--- 2023/2.1/h/src/Lib.hs
+++ 2023/2.1/h/src/Lib.hs
@@ -1,6 +1,75 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( Bag (..),
+    parseGame,
+    isGamePossible,
+  )
+where
+
+import qualified Data.Text as T
+
+data Colors = Colors
+  { redC :: !T.Text,
+    greenC :: !T.Text,
+    blueC :: !T.Text
+  }
+
+colors :: Colors
+colors = Colors "red" "green" "blue"
+
+data Bag = Bag
+  { red :: !Int,
+    green :: !Int,
+    blue :: !Int
+  }
+
+data Game = Game
+  { idNum :: !Int,
+    sets :: ![Bag]
+  }
+
+parseGame :: T.Text -> Game
+parseGame line =
+  Game new_id bag_list
+  where
+    new_id = parseGameId c
+    bag_list = map parseGameSet set
+    (c, s) = T.breakOn ":" line
+    set = T.splitOn ";" (T.drop 2 s)
+
+parseGameId :: T.Text -> Int
+parseGameId substr =
+  read (T.unpack (T.strip s)) :: Int
+  where
+    (_, s) = T.breakOn " " substr
+
+parseGameSet :: T.Text -> Bag
+parseGameSet substr =
+  Bag r g b
+  where
+    s = map T.strip (T.splitOn "," substr)
+    r = foldr ((+) . parseColor (redC colors)) 0 s
+    g = foldr ((+) . parseColor (greenC colors)) 0 s
+    b = foldr ((+) . parseColor (blueC colors)) 0 s
+
+parseColor :: T.Text -> T.Text -> Int
+parseColor color substr =
+  if T.isInfixOf color substr
+    then read (T.unpack (T.strip s)) :: Int
+    else 0
+  where
+    (s, _) = T.breakOn " " substr
+
+isGamePossible :: Bag -> Game -> Bool
+isGamePossible b g =
+  and bools
+  where
+    bools = map (isSetPossible b) (sets g)
+
+isSetPossible :: Bag -> Bag -> Bool
+isSetPossible b s =
+  and (zipWith (>=) bs ss)
+  where
+    bs = [red, green, blue] <*> [b]
+    ss = [red, green, blue] <*> [s]
