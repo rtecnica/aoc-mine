8bd538e59ef48506f95203bbd945908ce4c3ac5c
rtecnica <imaldonado@ing.uchile.cl>
2026-04-29 14:58:12 -0400

    Minor fixes

--- 2025/3.1-2/c/inc/lib.h
+++ 2025/3.1-2/c/inc/lib.h
@@ -1,6 +1,3 @@
 #include <stdint.h>
-#define IN(btm, top, item) ((item <= top) && (item >= btm) && (top >= btm))
 
-void bubbleSort(uint8_t* array, uint8_t size);
-void bump_insert(uint8_t* array, uint8_t size, uint8_t value, int index);
 uint64_t get_max_joltage(char* bank, uint32_t num_batt);
--- 2025/3.1-2/c/src/lib.c
+++ 2025/3.1-2/c/src/lib.c
@@ -4,38 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(uint8_t* xp, uint8_t* yp) {
-  uint8_t temp = *xp;
-  *xp = *yp;
-  *yp = temp;
-}
-
-void bubbleSort(uint8_t* array, uint8_t size) {
-  if (size < 2) {
-    return;
-  }
-  uint8_t i, j;
-  uint8_t swapped;
-  for (i = 0; i < (size - 1); i++) {
-    swapped = 0;
-    for (j = 0; j < (size - i - 1); j++) {
-      if (array[j] > array[j + 1]) {
-        swap(&array[j], &array[j + 1]);
-        swapped = 1;
-      }
-    }
-    if (swapped == 0)
-      break;
-  }
-}
-
-void bump_insert(uint8_t* array, uint8_t size, uint8_t value, int index) {
-  for (int i = size - 1; i > index; i--) {
-    swap(&array[i], &array[i - 1]);
-  }
-  array[index] = value;
-}
-
 uint64_t get_max_joltage(char* bank, uint32_t num_batt) {
   char* cursor = bank;
   uint8_t joltage[num_batt];  // index = order of magnitud
